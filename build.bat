18be59f3086a118d75d8b298c022bee940745c42
EthanReed517 <ethanreed517@gmail.com>
2024-12-30 13:26:40 -0600

    Add personal folder detection files for storage

--- build.bat
+++ build.bat
@@ -3,7 +3,11 @@ echo Clearing existing folder...
 if exist dist del /q dist
 echo Copying resources...
 robocopy >nul Scripts dist\Scripts
-robocopy >nul "Folder Detection" "dist\Folder Detection"
+echo 1) Personal Build
+echo 2) Release Build
+choice /c 12 /m "Which build are you doing?"
+if %errorlevel% equ 2 robocopy >nul "Folder Detection" "dist\Folder Detection"
+if %errorlevel% equ 1 robocopy >nul "Folder Detection (Personal)" "dist\Folder Detection"
 copy >nul settings.ini dist
 echo Running pyInstaller...
 pyinstaller igbFinisher.py --onefile --additional-hooks-dir=. --icon=icon.ico --add-data "icon.ico:." --add-data "images/dropZone.png:images"
