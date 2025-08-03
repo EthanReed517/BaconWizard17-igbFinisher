f8795dc8682e4f843ddeea353efe822bf1f6e26c
Ethan <ethanreed517@gmail.com>
2025-08-03 14:00:04 -0500

    Batch Call
    Rewrite the batch script that calls the function

--- igbFinisher.bat
+++ igbFinisher.bat
@@ -1,4 +1,31 @@
 @echo off
-REM call the python file
-py igbFinisher.py "C:\Users\ethan\Desktop\New Folder\test.igb"
+set "cmdpath=%~dp0"
+setlocal enableDelayedExpansion
+set "cmd=!cmdcmdline!"
+set "cmd2=!cmd:*%~f0=!"
+if "!cmd2!" == "!cmd!" goto isbatch
+endlocal
+for %%p in (%*) do 2>nul pushd "%~1" && goto isfolder || goto isfiles
+
+:isbatch
+endlocal
+for /R %%f in (*.igb) do (
+    py "%cmdpath%\igbFinisher.py" "%%~f"
+)
+pause
+goto eof
+
+:isfolder
+if (%1) == () goto eof
+for %%f in ("%~1\*.igb") do (
+    py "%cmdpath%\igbFinisher.py" "%%~f"
+)
+pause
+shift
+goto isfolder
+
+:isfiles
+for %%f in (%*) do (
+    py "%cmdpath%\igbFinisher.py" "%%~f"
+)
 pause
