3b6df31bb80b842aabec057b9d43d262be9a7ac8
Ethan <ethanreed517@gmail.com>
2025-08-07 18:02:46 -0500

    Remove old texture functions
    Remove the old functions related to textures.

--- textures2/__init__.py
+++
@@ -1,6 +0,0 @@
-from .textureFormats3D import *
-from .textureFormatsComic import *
-from .textureFormatsConvo import *
-from .textureFormatsCSP import *
-from .textureFormatsIcons import *
-from .textureFormatsLoad import *
